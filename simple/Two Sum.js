42ac5d6d71c500299156c87b81f2a5f5128e00eb
hulijuan <279062301@qq.com>
2019-03-18 20:59:00 +0800

    添加版本

--- simple/Two Sum.js
+++ simple/Two Sum.js
@@ -1,1 +1,20 @@
 //https://leetcode.com/problems/two-sum/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var len = nums.length;
+    var result = []
+    nums.map(function(item,index){
+        var rest = target - item;
+        for(var i=index+1;i<len;i++){
+            if( rest == nums[i]){
+                result = [index,i]
+                return false;
+            }
+        }
+    });
+    return result;
+};
