a463c1f19735bacabfa3b17574e02661f97c44cc
hulijuan <279062301@qq.com>
2019-03-18 21:16:10 +0800

    修改实现方式

--- simple/Two Sum.js
+++ simple/Two Sum.js
@@ -6,15 +6,14 @@
  */
 var twoSum = function(nums, target) {
     var len = nums.length;
-    var result = []
-    nums.map(function(item,index){
+    var obj = {};
+    for(var j=0;j<len;j++){
+        var item = nums[j];
         var rest = target - item;
-        for(var i=index+1;i<len;i++){
-            if( rest == nums[i]){
-                result = [index,i]
-                return false;
-            }
+        if(obj.hasOwnProperty(item)){
+            return [obj[item],j]
+        }else{
+            obj[rest] = j;
         }
-    });
-    return result;
+    }
 };
