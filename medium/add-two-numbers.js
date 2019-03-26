d584e63023e804beb5ae712167fda28015aba1a5
hulijuan <279062301@qq.com>
2019-03-26 11:25:10 +0800

    优化addTwoNumbers代码

--- medium/add-two-numbers.js
+++ medium/add-two-numbers.js
@@ -19,59 +19,24 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    var result = l1;
-    var resultList = [];
-    var temp1 = l1,temp2 = l2,add = false;
-    while( temp1 || temp2 || add){
-        var sum = 0;
-        if(add){
-            sum++;
-        }
-        if(temp1 && temp2){
-            sum += temp1.val+temp2.val;
-            if( sum > 9){
-                resultList.push(sum%10);
-                add = true;
-            }else{
-                resultList.push(sum);
-                add = false;
-            }
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-        }else if(temp1 && !temp2){
-           sum += temp1.val;
-            if( sum > 9){
-                  resultList.push(sum%10);
-                add = true;
-            }else{
-                resultList.push(sum);
-                add = false;
-            }
-           temp1 = temp1.next;
-        }else if(temp2 && !temp1){
-           sum += temp2.val; 
-            if( sum > 9){
-                resultList.push(sum%10);
-                add = true;
-            }else{
-                resultList.push(sum);
-                add = false;
-            }
-           temp2 = temp2.next;
-        }else if(sum){
-            resultList.push(sum);
-            add = false;
-            break
-        }else{
-             break;
-        }
+    var target = new ListNode(0);
+    var result = target;
+    
+    var temp1 = l1,temp2 = l2;
+    var temp1val = 0,temp2val = 0,sum = 0;
+    while( temp1 || temp2 || sum){
+        temp1val = temp1 ? temp1.val : 0;
+        temp2val = temp2 ? temp2.val : 0;
+        sum += temp1val + temp2val;
+        
+        target.next = new ListNode(0);
+        target = target.next;
+        target.val = sum%10;
+        
+        sum = Math.floor(sum/10);
+        
+        temp1 = temp1 ? temp1.next:null;
+        temp2 = temp2 ? temp2.next:null;
     }
-    var next = null;
-    var listLen = resultList.length;
-    for(i=listLen-1;i>=0;i--){
-        var newObj = new ListNode(resultList[i]);
-        newObj.next = next;
-        next = newObj;
-    }
-    return newObj;
+    return result.next;
 };
